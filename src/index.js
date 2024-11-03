36da1c14704e534853c6d88ae17288a2abc8283c
Mateus Santos <54743403+mateusdata@users.noreply.github.com>
2024-11-03 16:52:03 +0000

    fix: remove QR code of chrome

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-const express = require('express');
-const path = require('path');
+// index.js
 const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const frases = require("./frases");
 const puppeteer = require('puppeteer');
-
-const app = express();
-const port = 3000;
-
-// Serve arquivos estáticos da pasta public
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Rota para a página principal
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 // Configuração do cliente do WhatsApp com Puppeteer
 const client = new Client({
@@ -26,63 +14,62 @@ const client = new Client({
 });
 
 client.on("qr", (qr) => {
-    // Aqui, você pode decidir se quer mostrar no terminal ou no HTML
-    console.log("QR Code gerado");
-    qrcode.generate(qr, { small: true }); // Remova essa linha se não quiser ver no terminal
+  qrcode.generate(qr, { small: true });
 });
 
 client.on("ready", () => {
-    console.log("Client is ready!");
+  console.log("Client is ready!");
 });
 
 const greetedUsers = new Set();
 const conversationsEnded = new Set();
 
 client.on("message", (message) => {
-    if (message.isGroupMsg) return;
+  if (message.isGroupMsg) return;
 
-    const userMessage = message.body.trim().toLowerCase();
+  const userMessage = message.body.trim().toLowerCase();
 
-    if (conversationsEnded.has(message.from)) {
-        client.sendMessage(message.from, frases.Finalizarconversaaceitou);
-        return;
-    }
+  if (conversationsEnded.has(message.from)) {
+    client.sendMessage(message.from, frases.Finalizarconversaaceitou);
+    return;
+  }
 
-    if (!greetedUsers.has(message.from)) {
-        client.sendMessage(message.from, frases.Menu);
-        greetedUsers.add(message.from);
-    }
+  if (!greetedUsers.has(message.from)) {
+    client.sendMessage(message.from, frases.Menu);
+    greetedUsers.add(message.from);
+  }
 
-    // Resposta ao pressionar "0"
-    if (userMessage === "0") {
-        client.sendMessage(message.from, frases.Menu + frases.VoltarAoMenu);
-        return;
-    } else if (userMessage === "1") {
-        client.sendMessage(message.from, frases.Agendamento + frases.VoltarAoMenu);
-        setTimeout(() => {
-            client.sendMessage(message.from, frases.Finalizarconversa + frases.PausaFinalizarConversa);
-            conversationsEnded.add(message.from);
-        }, 10); // Pausa de 60 minutos
-        return;
-    } else if (userMessage === "2") {
-        client.sendMessage(message.from, frases.Informacoes + frases.VoltarAoMenu);
-        setTimeout(() => {
-            client.sendMessage(message.from, frases.Finalizarconversa + frases.PausaFinalizarConversa);
-            conversationsEnded.add(message.from);
-        }, 10); // Pausa de 60 minutos
-        return;
-    } else if (userMessage === "3") {
-        client.sendMessage(message.from, frases.PortfolioServicos + frases.VoltarAoMenu);
-        return;
-    } else if (userMessage === "fim") {
-        conversationsEnded.add(message.from);
-        client.sendMessage(message.from, frases.Finalizarconversa + frases.PausaFinalizarConversa);
-        return;
-    }
+  // Resposta ao pressionar "0"
+  if (userMessage === "0") {
+    client.sendMessage(message.from, frases.Menu + frases.VoltarAoMenu);
+    return;
+  } else if (userMessage === "1") {
+    client.sendMessage(message.from, frases.Agendamento + frases.VoltarAoMenu);
+    setTimeout(() => {
+      client.sendMessage(message.from, frases.Finalizarconversa + frases.PausaFinalizarConversa);
+      conversationsEnded.add(message.from);
+    }, 10); // Pausa de 60 minutos
+    return;
+  } else if (userMessage === "2") {
+    client.sendMessage(message.from, frases.Informacoes + frases.VoltarAoMenu);
+    setTimeout(() => {
+      client.sendMessage(message.from, frases.Finalizarconversa + frases.PausaFinalizarConversa);
+      conversationsEnded.add(message.from);
+    }, 10); // Pausa de 60 minutos
+    return;
+  } else if (userMessage === "3") {
+    client.sendMessage(message.from, frases.PortfolioServicos + frases.VoltarAoMenu);
+    return;
+  } else if (userMessage === "fim") {
+    conversationsEnded.add(message.from);
+    client.sendMessage(message.from, frases.Finalizarconversa + frases.PausaFinalizarConversa);
+    return;
+  }
 });
 
 client.initialize();
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+
+
+
+
